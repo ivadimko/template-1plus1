c59f01396367b0a72b9063ba135a643609373891
Vadim Ilchenko <vvadimko@gmail.com>
2017-10-08 00:27:43 +0300

    finish section

--- dev/assets/scripts/common.js
+++ dev/assets/scripts/common.js
@@ -2,7 +2,6 @@ $(document).foundation();
 $(window).on('load', () => {
 	switchTabs('.js__tab', '.js__tab-content');
 	switchTabs('.filter__value');
-	//alert('hi');
 });
 function switchTabs(selectorTab, selectorContent) {
 	$(selectorTab).eq(0).addClass('active');
